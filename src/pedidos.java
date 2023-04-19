bd301231f4d222263728b4267f291249603c1fb9
gutierrez <juliangutierrezor@gmail.com>
2023-04-19 16:56:38 -0500

    interfaz de pedido
    se creo la interfaz de pedido

---
+++ src/pedidos.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class pedidos {
+    private JButton enviarPedidoButton;
+
+    private JButton cancelarPedidoButton;
+    private JTextField nombre;
+    private JTextField id;
+    private JTextField telefono;
+    private JComboBox colores;
+}
