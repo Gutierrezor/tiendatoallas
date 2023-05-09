4b4cacf9b30a51ed76a9b05ec3c1df74726d9491
gutierrez <juliangutierrezor@gmail.com>
2023-05-09 17:09:32 -0500

    springbot
    tuve problemas con archivos internos del proyecto
    
    Co-Authored-By: JuanJoseClarosGonzalez <131219584+JuanJoseClarosGonzalez@users.noreply.github.com>

---
+++ src/main/java/com/example/tiendatoalla/TiendatoallaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiendatoalla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendatoallaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendatoallaApplication.class, args);
+	}
+
+}
