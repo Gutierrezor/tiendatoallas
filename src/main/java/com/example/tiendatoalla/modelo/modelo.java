eba6b00314c823d09b6cbf3af6da539ff51aa8b7
gutierrez <juliangutierrezor@gmail.com>
2023-05-09 20:51:39 -0500

    modelos
    se crearon los modelos con las tablas en la base de datos}
    
    Co-Authored-By: JuanJoseClarosGonzalez <131219584+JuanJoseClarosGonzalez@users.noreply.github.com>

---
+++ src/main/java/com/example/tiendatoalla/modelo/modelo.java
@@ -0,0 +1,35 @@
+package com.example.tiendatoalla.modelo;
+import javax.persistence.*;
+import javax.xml.crypto.Data;
+
+@Entity
+@Table(name = "modelo")
+public class modelo {
+    @Id
+    @Column(name = "nombre")
+    private String nombre;
+    @Column (name = "cedula")
+    private  String cedula;
+
+    public modelo( String nombre, String cedula) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
+        public String getCedula() {
+            return cedula;
+        }
+
+        public void setId(String cedula) {
+            this.cedula = cedula;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setName(String nombre) {
+            this.nombre = nombre;
+        }
+
+
+}
